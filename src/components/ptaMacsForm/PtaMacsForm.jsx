532abfdbae1c3981117209af9cb0194376056b99
Shawn Kebel <fxrender@outlook.com>
2023-10-25 20:45:03 -0500

    :building_construction: Remove `Sidebar` from Order Form  page

--- src/components/ptaMacsForm/PtaMacsForm.jsx
+++ src/components/ptaMacsForm/PtaMacsForm.jsx
@@ -9,16 +9,15 @@ const PtaMacsForm = () => {
     <>
       <Topbar />
       <div className="flex w-full">
-        <Leftbar />
         <div className="flex-[6]">
           <div className="mt-[30px] px-[25px] pb-[25px]">
             <div className="bg-[#0000000d] pt-[16px] pb-[32px] px-[16px] border-b-[2px] border-b-[#999]">
               <Row className="flex justify-center">
-                <h2 className="text-xl font-semibold mt-0 mb-[16px] mx-0">Order your PTA MACS</h2>
+                <h2 className="text-xl font-semibold mt-0 mb-[16px] mx-0">
+                  Order your <span className="text-[blue]">PTA MACS</span>
+                </h2>
               </Row>
-              {/* <Form> */}
               <iframe src={PTAmacs} frameborder="0" width="100%" height="1200px"></iframe>
-              {/* </Form> */}
             </div>
           </div>
         </div>
