80f8d10d79e0737567ecb5e73c9ec8a29e38a4bd
Shawn Kebel <fxrender@outlook.com>
2023-10-25 19:49:54 -0500

    :sparkles: Add PTA MACS `order form`

--- src/components/ptaMacsForm/PtaMacsForm.jsx
+++ src/components/ptaMacsForm/PtaMacsForm.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Topbar from "../topbar/Topbar";
 import Leftbar from "../leftbar/Leftbar";
-import { Form, Row } from "reactstrap";
+import { Row } from "reactstrap";
+import PTAmacs from "../pdf/Fillable MACS Form 3rd Edition[1069].pdf";
 
 const PtaMacsForm = () => {
   return (
@@ -13,9 +14,11 @@ const PtaMacsForm = () => {
           <div className="mt-[30px] px-[25px] pb-[25px]">
             <div className="bg-[#0000000d] pt-[16px] pb-[32px] px-[16px] border-b-[2px] border-b-[#999]">
               <Row className="flex justify-center">
-                <h2 className="text-xl font-semibold mt-0 mb-[16px] mx-0">Order PTA MACs Binder</h2>
+                <h2 className="text-xl font-semibold mt-0 mb-[16px] mx-0">Order your PTA MACS</h2>
               </Row>
-              <Form>Form</Form>
+              {/* <Form> */}
+              <iframe src={PTAmacs} frameborder="0" width="100%" height="1200px"></iframe>
+              {/* </Form> */}
             </div>
           </div>
         </div>
