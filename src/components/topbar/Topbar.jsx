104a73664afdeccd2b7f7973305f976f0ed8c374
Shawn Kebel <fxrender@outlook.com>
2024-01-15 20:22:13 -0600

    :sparkles: Update Topbar component with new logo and added Contact Us link

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import { NavLink } from "react-router-dom";
 
-import logo from "../../assets/images/texas-logo.png";
+import logo from "../../assets/images/texaslogo-01.png";
 
 const nav__links = [
   {
@@ -34,24 +34,33 @@ const nav__links = [
     path: "/upcoming-ccus",
     display: "Upcoming CCUs",
   },
+  {
+    path: "/contact-us",
+    display: "Contact Us",
+  },
 ];
 
 export default function Topbar() {
   const menuRef = useRef(null);
   const toggleMenu = () => menuRef.current.classList.toggle("show__menu");
+
   return (
-    <div className="h-[75px] w-full flex items-center justify-center sticky top-0 z-[999] bg-[#27335933] rounded-2xl shadow-md backdrop-blur-[15px] border border-solid border-[#0000004d]">
-      <div className="absolute left-0">
+    <div className="h-[75px] w-full flex items-center justify-center sticky top-0 z-[999] bg-[#e0e6f6] rounded-2xl shadow-md backdrop-blur-[15px] border border-solid border-[#0000004d]">
+      <div className="absolute left-0 ">
         <div className="w-[70px] ml-[12px]">
           <img
-            className="border-[2px] border-[#d06767] rounded-3xl pr-1 cursor-pointer"
+            className="border-[2px] border-[#d06767] rounded-3xl pr-1"
             src={logo}
             alt=""
           />
         </div>
       </div>
 
-      <div className="navigation ml-[64px]" ref={menuRef} onClick={toggleMenu}>
+      <div
+        className="navigation ml-[64px] justify-center"
+        ref={menuRef}
+        onClick={toggleMenu}
+      >
         <ul className="flex mb-0 items-center gap-3">
           {nav__links.map((item, index) => (
             <li className="nav__item" key={index}>
