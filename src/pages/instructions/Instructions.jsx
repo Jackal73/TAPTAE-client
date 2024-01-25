e6fe5ccc5fc931560abdaefbac229b5842c4ea1e
Shawn Kebel <fxrender@outlook.com>
2024-01-25 12:13:43 -0600

    :sparkles::construction: Add Instructions component

---
+++ src/pages/instructions/Instructions.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PtaMacsInstructions from "../../components/ptaMacsInstructions/PtaMacsInstructions";
+
+const Instructions = () => {
+  return (
+    <div>
+      <PtaMacsInstructions />
+    </div>
+  );
+};
+
+export default Instructions;
