ba126b43abf793e52def123c561c64ff51b017f9
Shawn Kebel <fxrender@outlook.com>
2024-01-25 12:09:44 -0600

    :lipstick: Fix hover color in schoolLink

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,7 +1,7 @@
 const styles = {
   affiliate:
     "mb-[6px] border border-transparent hover:rounded-sm hover:text-[blue] pb-[2px]",
-  schoolLink: "no-underline text-[12px] font-semibold hover:text-[blue]",
+  schoolLink: "no-underline text-[12px] font-semibold !hover:text-[blue]",
   // shortUrl:
   //   "text-[#555] font-semibold pl-3 text-[10px] hover:text-[blue] cursor-pointer",
   shortUrl: "text-[#555] text-[10px]",
