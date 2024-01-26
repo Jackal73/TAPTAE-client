039c7fc42875e5578f50af02f027ef7aac7a624b
Shawn Kebel <fxrender@outlook.com>
2024-01-26 09:20:49 -0600

    :necktie: Update PTA MACS instructions PDF file name

--- src/components/ptaMacsInstructions/PtaMacsInstructions.jsx
+++ src/components/ptaMacsInstructions/PtaMacsInstructions.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Topbar from "../topbar/Topbar";
-import pdf from "../../assets/pdf/instructions.pdf";
+import pdf from "../../assets/pdf/Instructions.pdf";
 
 const PtaMacsInstructions = () => {
   return (
@@ -8,13 +8,21 @@ const PtaMacsInstructions = () => {
       <div className="hidden sm:flex">
         <Topbar />
       </div>
-      <div className="flex text-4xl text-blue-700 mt-[24px] mb-[12px] justify-center">
-        PTA MACS Instructions
+      <div className=" w-full h-[calc(100vh-76px)">
+        <div className="h-[24px]"></div>
+        <div className="flex text-4xl text-[white] mt-[100px] mb-[6px] justify-center">
+          PTA MACS Instructions
+        </div>
+        <div className="flex text-xl text-[white] mb-[24px] justify-center">
+          (Downloadable and Printable)
+        </div>
+        <iframe
+          src={pdf}
+          width="90%"
+          height="1366px"
+          className="mx-auto 1200px:w-[80%] 1800px:w-[60%]"
+        />
       </div>
-      <div className="flex text-xl text-blue-700 my-[12px] justify-center">
-        (Downloadable and Printable)
-      </div>
-      <iframe src={pdf} width="100%" height="768px" />
     </>
   );
 };
