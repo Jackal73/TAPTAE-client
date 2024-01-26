b40896077dae196a960bc3f73ab2f483bcfc9d2a
Shawn Kebel <fxrender@outlook.com>
2024-01-26 09:21:51 -0600

    :lipstick: Update styles and colors in `Home.jsx`

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,19 +14,19 @@ export default function Home() {
       <main className="opacity-[6]">
         <div className="flex w-full">
           <Leftbar />
-          <div className="flex-[8] main-container ">
+          <div id="right-pane" className="">
             <div className="pt-[10px] px-[25px] pb-[25px]">
               <Row>
-                <Col className="flex">
-                  <h1 className="text-center font-bold mt-32 mb-[16px] mx-auto text-[3.5rem]">
+                <Col className="flex w-[85%] mx-auto">
+                  <h1 className="text-center font-bold mt-32 mb-[16px] mx-auto text-[3.5rem] text-[blue]">
                     Texas Alliance of Physical Therapist Assistant Educators
-                    <br /> (<span className="text-[blue]">TAPTAE</span>)
+                    <br /> (<span className="">TAPTAE</span>)
                   </h1>
                 </Col>
               </Row>
               <Row className="z-[99]">
-                <Col className="flex">
-                  <h1 className="text-center text-gray-900 text-4xl font-semibold mt-10 mb-[24px] mx-auto">
+                <Col className="flex w-[85%] mx-auto">
+                  <h1 className="text-center text-[black] font-outline-2 text-4xl font-bold mt-10 mb-[24px] mx-auto">
                     <i>
                       Promoting collaboration and quality education
                       <br /> in PTA Programs across Texas
@@ -40,7 +40,7 @@ export default function Home() {
                   <Link
                     to="/ptaMacsForm"
                     // href=""
-                    className="text-blue-700 border-2 p-2 font-semibold rounded-sm border-[blue] transition-all shadow-md shadow-black/70 hover:bg-[blue] hover:text-white bg-white/70 hover:!shadow-none"
+                    className="text-[white] border-2 p-2 font-semibold rounded-sm border-[blue] transition-all shadow-md shadow-black/70 hover:bg-white/70 hover:text-[blue] bg-[blue] hover:!shadow-none"
                     style={{ textDecoration: "none" }}
                   >
                     Order your PTA MACS here
